9645cc0ec1833c00b717b826b747ddac85d423d7
OlegPylypchuk2004 <oleg.pylypchuk2004@gmail.com>
2026-03-31 12:43:47 +0300

    Update MainSceneEntryPoint.cs

--- Assets/Game/Scripts/Scenes/Main/MainSceneEntryPoint.cs
+++ Assets/Game/Scripts/Scenes/Main/MainSceneEntryPoint.cs
@@ -1,11 +1,14 @@
 using Scenes.Main.StateMachine;
 using Scenes.Main.StateMachine.States;
+using UnityEngine;
 using Zenject;
 
 namespace Scenes.Main
 {
     public class MainSceneEntryPoint : SceneEntryPoint
     {
+        private const double DefaultFrameRate = 60;
+
         private MainSceneStateMachine _sceneStateMachine;
 
         [Inject]
@@ -16,6 +19,20 @@ namespace Scenes.Main
 
         protected override void Start()
         {
+            QualitySettings.vSyncCount = 0;
+
+            double targetFrameRate = DefaultFrameRate;
+
+            foreach (Resolution screenResolution in Screen.resolutions)
+            {
+                if (screenResolution.refreshRateRatio.value > DefaultFrameRate)
+                {
+                    targetFrameRate = screenResolution.refreshRateRatio.value;
+                }
+            }
+
+            Application.targetFrameRate = (int)targetFrameRate;
+
             _sceneStateMachine.ChangeState<IntroState>();
         }
     }
